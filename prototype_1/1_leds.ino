c76ed7db5c0fe628736dd16d606d42a7f7ee5842
Jan Schneider <jan.schneider@hfg-gmuend.de>
2019-09-25 14:41:26 +0200

    third state with nearly off led + fading deleted

--- prototype_1/1_leds.ino
+++ prototype_1/1_leds.ino
@@ -16,14 +16,9 @@ int lam_led_brightness = 0;
 
 //Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
 
-unsigned long led_previousMillis = 0;
-const long led_normal_interval = 700;
-
 boolean ledsOn = false;
-boolean fadeUp = true;
 
 int led_status = 0;
-boolean newLedStatusReached = false;
 
 const int led_status_idle = 0;
 const int led_status_highlighted = 1;
@@ -82,31 +77,6 @@ void ledOff() {
   FastLED.show();
 }
 
-void fadeLed() {
-  unsigned long led_currentMillis = millis();
-  if (led_currentMillis - led_previousMillis >= led_normal_interval) {
-    led_previousMillis = led_currentMillis;
-    
-    if (5 >= lam_led_brightness) {
-      adjustLedBrightness(0);
-    } else if (40 <= lam_led_brightness && !fadeUp) {
-      fadeUp = true;
-    } else if (40 <= lam_led_brightness && 60 >= lam_led_brightness) {
-      adjustLedBrightness(1);
-      Serial.println("e");
-    } else if (60 >= lam_led_brightness && fadeUp) {
-      fadeUp = false;
-    } else if (60 >= lam_led_brightness) {
-      adjustLedBrightness(-1);
-      Serial.println("üüüe");
-    } else {
-      adjustLedBrightness(0);
-    }
-    setLedBrightness();
-    Serial.println(lam_led_brightness);
-  }
-}
-
 void setLedBrightness() {
   for (int i = 0; i < lam_num_leds; i++)
     leds[i].setHSV(lam_led_hue, 255, lam_led_brightness);
@@ -118,60 +88,58 @@ void adjustLedBrightness(int a) {
 }
 
 void checkLedBrightness() {
+  Serial.println(led_status);
   switch (led_status) {
     case led_status_idle :
       if (5 < lam_led_brightness) {
-        newLedStatusReached = false;
         adjustLedBrightness(-5);
       } else {
         ledOff();
         lam_led_brightness = 0;
-        newLedStatusReached = true;
       }
       break;
     case led_status_highlighted :
-      if (50 > lam_led_brightness && !newLedStatusReached) {
+      if(50 > lam_led_brightness) {
         adjustLedBrightness(1);
-        newLedStatusReached = false;
-      } else if (50 < lam_led_brightness && !newLedStatusReached) {
+      } else if(50 < lam_led_brightness) {
         adjustLedBrightness(-5);
-        newLedStatusReached = false;
-      } else {
-        newLedStatusReached = true;
       }
-      break;
-    case led_status_inUse :
-      if (250 > lam_led_brightness) {
+    break;
+    case led_status_attracted :
+      if(250 > lam_led_brightness) {
         adjustLedBrightness(5);
-        newLedStatusReached = false;
-      } else {
-        newLedStatusReached = true;
       }
-      break;
+    break;
+    case led_status_inUse : 
+    Serial.println("hi");
+      if (15 < lam_led_brightness) {
+        adjustLedBrightness(-5);
+      } else {
+        lam_led_brightness = 10;
+      }
+    break;
     default:
       Serial.println("error");
       break;
   }
   setLedBrightness();
-  if (newLedStatusReached) {
-    //fadeLed();
-  }
 }
 
 void setLedStatus() {
+  //Serial.println(lam_led_brightness);
   switch (distance) {
     case -1 :
       led_status = led_status_idle;
+      ledOff();
       break;
     default :
       if (50 >= distance) {
         led_status = led_status_inUse;
       } else if (200 >= distance) {
+        led_status = led_status_attracted;
+      } else if (500 >= distance) {
         led_status = led_status_highlighted;
       }
       break;
   }
 }
-
-
-// ---------------------------------------------- Acting Machine control flow
