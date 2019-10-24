074b2a773324f060b6a104e7da77ea6c81960a14
Jan Schneider <jan.schneider@hfg-gmuend.de>
2019-10-24 17:49:31 +0200

    basic arduino mqtt code start

---
+++ prototype_4/arduino/arduino.ino
@@ -0,0 +1,81 @@
+// Main program file for prototype 2 (luggage finding support)
+//
+// Uses (check the tabs of your Arduino window):
+//   2_users.ino    - user detection
+//   2_database.ino - for the user / luggage database
+//   2_led-matrix   - for luggage highlighting
+//   debug.ino    - for Serial and debugging
+//
+// Created October 2019
+// By Jan Schneider
+
+#include <WiFiNINA.h>
+#include <MQTT.h>
+
+//wifi settings
+const char ssid[] = "TUvisitor";
+const char pass[] = "pass";
+
+//mqtt settings
+const char mqtt_clientID[] = "Arduino Nano IOT";
+const char mqtt_username[] = "connected-thing";
+const char mqtt_password[] = "c784e41dd3da48d4";
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("+");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect(mqtt_clientID, mqtt_username, mqtt_password)) {
+    Serial.print("x");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/jan");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("WiFi.begin");
+  WiFi.begin(ssid);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  //
+  // MQTT brokers usually use port 8883 for secure connections.
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
+
+}
+
+void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/jan", "world");
+  }
+}
